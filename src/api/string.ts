0ea96e8905ac3a0c71d0c3170b32096fb78a0e43
Rauli Laine <rauli.laine@iki.fi>
2018-01-16 21:57:17 +0200

    Implement rest of string prototype

--- src/api/string.ts
+++ src/api/string.ts
@@ -1,57 +1,119 @@
 import Context from "../context";
 import { PrototypeDefinition } from "../runtime";
 
+import {
+  PlorthErrorCode,
+  PlorthNumber,
+  PlorthValue,
+  PlorthValueType,
+  PlorthString
+} from "plorth-types";
+
 const StringPrototype: PrototypeDefinition = {
   length(context: Context) {
     context.pushNumber(context.peekString().length);
   },
 
   chars(context: Context) {
-    // TODO
+    const string = context.peekString();
+    const result: PlorthValue[] = [];
+
+    for (let i = 0; i < string.length; ++i) {
+      result.push({
+        type: PlorthValueType.STRING,
+        value: string[i]
+      } as PlorthString);
+    }
+    context.pushArray(...result);
   },
 
   runes(context: Context) {
-    // TODO
+    const string = context.peekString();
+    const result: PlorthValue[] = [];
+
+    for (let i = 0; i < string.length; ++i) {
+      result.push({
+        type: PlorthValueType.NUMBER,
+        value: string.charCodeAt(i)
+      } as PlorthNumber);
+    }
+    context.pushArray(...result);
   },
 
   words(context: Context) {
-    // TODO
+    context.pushArray(...context.peekString().split(/\s+/).map(word => ({
+      type: PlorthValueType.STRING,
+      value: word
+    }) as PlorthString));
   },
 
   lines(context: Context) {
-    // TODO
-  },
-
-  "includes?"(context: Context) {
-    // TODO
-  },
-
-  "index-of"(context: Context) {
-    // TODO
-  },
-
-  "starts-with?"(context: Context) {
-    // TODO
-  },
-
-  "ends-with?"(context: Context) {
-    // TODO
+    context.pushArray(...context.peekString().split(/(\r?\n)+/).map(line => ({
+      type: PlorthValueType.STRING,
+      value: line
+    }) as PlorthString));
   },
 
   "space?"(context: Context) {
-    // TODO
+    const string = context.peekString();
+
+    if (!string.length) {
+      context.pushBoolean(false);
+      return;
+    }
+    for (let i = 0; i < string.length; ++i) {
+      if (!/\s/.test(string.charAt(i))) {
+        context.pushBoolean(false);
+        return;
+      }
+    }
+    context.pushBoolean(true);
   },
 
   "lower-case?"(context: Context) {
-    // TODO
+    const string = context.peekString();
+
+    if (!string.length) {
+      context.pushBoolean(false);
+      return;
+    }
+    for (let i = 0; i < string.length; ++i) {
+      const c = string.charAt(i);
+
+      if (c !== c.toLowerCase()) {
+        context.pushBoolean(false);
+        return;
+      }
+    }
+    context.pushBoolean(true);
   },
 
   "upper-case?"(context: Context) {
-    // TODO
+    const string = context.peekString();
+
+    if (!string.length) {
+      context.pushBoolean(false);
+      return;
+    }
+    for (let i = 0; i < string.length; ++i) {
+      const c = string.charAt(i);
+
+      if (c !== c.toUpperCase()) {
+        context.pushBoolean(false);
+        return;
+      }
+    }
+    context.pushBoolean(true);
   },
 
   reverse(context: Context) {
-    // TODO
+    const string = context.popString();
+    let result = "";
+
+    for (let i = string.length; i > 0; --i) {
+      result += string.charAt(i - 1);
+    }
+    context.pushString(result);
   },
 
   "upper-case"(context: Context) {
@@ -63,12 +125,25 @@ const StringPrototype: PrototypeDefinition = {
   },
 
   "swap-case"(context: Context) {
-    // TODO
+    const string = context.popString();
+    let result = "";
+
+    for (let i = 0; i < string.length; ++i) {
+      let c = string.charAt(i);
+
+      if (c === c.toUpperCase()) {
+        c = c.toLowerCase();
+      } else if (c === c.toLowerCase()) {
+        c = c.toUpperCase();
+      }
+      result += c;
+    }
+    context.pushString(result);
   },
 
   capitalize(context: Context) {
     const string = context.popString();
-    let result = '';
+    let result = "";
 
     for (let i = 0; i < string.length; ++i) {
       const c = string[i];
@@ -83,43 +158,28 @@ const StringPrototype: PrototypeDefinition = {
   },
 
   trim(context: Context) {
-    // TODO
+    context.pushString(context.popString().trim());
   },
 
   "trim-left"(context: Context) {
-    // TODO
+    context.pushString(context.popString().replace(/^\s+/, ""));
   },
 
   "trim-right"(context: Context) {
-    // TODO
-  },
-
-  "pad-left"(context: Context) {
-    // TODO
-  },
-
-  "pad-right"(context: Context) {
-    // TODO
-  },
-
-  substring(context: Context) {
-    // TODO
-  },
-
-  split(context: Context) {
-    // TODO
-  },
-
-  replace(context: Context) {
-    // TODO
-  },
-
-  normalize(context: Context) {
-    // TODO
+    context.pushString(context.popString().replace(/\s+$/, ""));
   },
 
   ">number"(context: Context) {
-    // TODO
+    const input = context.popString();
+    const number = parseFloat(input);
+
+    if (isNaN(number)) {
+      throw context.error(
+        PlorthErrorCode.VALUE,
+        "Could not convert string to number."
+      );
+    }
+    context.pushNumber(number);
   },
 
   "+"(context: Context) {
@@ -130,11 +190,33 @@ const StringPrototype: PrototypeDefinition = {
   },
 
   "*"(context: Context) {
-    // TODO
+    const string = context.popString();
+    const count = context.popNumber();
+    let result = "";
+
+    if (count < 0) {
+      throw context.error(PlorthErrorCode.RANGE, "Invalid repeat count.");
+    }
+    for (let i = 0; i < count; ++i) {
+      result += string;
+    }
+    context.pushString(result);
   },
 
   "@"(context: Context) {
-    // TODO
+    const string = context.popString();
+    let index = context.popNumber();
+
+    if (index < 0) {
+      index += string.length;
+    }
+    if (!string.length || index < 0 || index >= string.length) {
+      throw context.error(
+        PlorthErrorCode.RANGE,
+        "String index out of bounds."
+      );
+    }
+    context.pushString(string.charAt(index));
   },
 
   ">symbol"(context: Context) {
