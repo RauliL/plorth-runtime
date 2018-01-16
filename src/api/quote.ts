91289b466a9258dc9116205dfd827af1ab863124
Rauli Laine <rauli.laine@iki.fi>
2018-01-16 19:52:38 +0200

    Implement quote prototype

--- src/api/quote.ts
+++ src/api/quote.ts
@@ -1,5 +1,6 @@
 import Context from "../context";
 import { PrototypeDefinition } from "../runtime";
+import { PlorthQuote, PlorthValueType } from "plorth-types";
 
 const QuotePrototype: PrototypeDefinition = {
   call(context: Context) {
@@ -7,23 +8,58 @@ const QuotePrototype: PrototypeDefinition = {
   },
 
   compose(context: Context) {
-    // TODO
+    const right = context.popQuote();
+    const left = context.popQuote();
+
+    context.push({
+      type: PlorthValueType.QUOTE,
+      callback(subContext: Context) {
+        subContext.call(left);
+        subContext.call(right);
+      }
+    } as PlorthQuote);
   },
 
   curry(context: Context) {
-    // TODO
+    const quote = context.popQuote();
+    const argument = context.pop();
+
+    context.push({
+      type: PlorthQuote,
+      callback(subContext: Context) {
+        subContext.push(argument);
+        subContext.call(quote);
+      }
+    } as PlorthQuote);
   },
 
   negate(context: Context) {
-    // TODO
+    const quote = context.popQuote();
+
+    context.push({
+      type: PlorthValueType.QUOTE,
+      callback(subContext: Context) {
+        subContext.call(quote);
+        subContext.pushBoolean(!subContext.popBoolean());
+      }
+    } as PlorthQuote);
   },
 
   dip(context: Context) {
-    // TODO
+    const quote = context.popQuote();
+    const value = context.pop();
+
+    context.call(quote);
+    context.push(value);
   },
 
   "2dip"(context: Context) {
-    // TODO
+    const quote = context.popQuote();
+    const value2 = context.pop();
+    const value1 = context.pop();
+
+    context.call(quote);
+    context.push(value1, value2);
   },
 
   ">word"(context: Context) {
