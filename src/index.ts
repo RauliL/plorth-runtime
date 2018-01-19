adb28848c752184ae4f9979826d686056c31790e
Rauli Laine <rauli.laine@iki.fi>
2018-01-19 19:16:16 +0200

    Export more classes and interfaces
    Instead of exporting only the `Runtime` class, include `Context`,
    `Importer` and `RuntimeError` in exports as well. Keep the `Runtime`
    class as default export though.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+import Context from "./context";
+import Importer from "./importer";
 import Runtime from "./runtime";
+import RuntimeError from "./error";
 
 export default Runtime;
+
+export {
+  Context,
+  Importer,
+  RuntimeError
+};
